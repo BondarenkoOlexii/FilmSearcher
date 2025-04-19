4a10b5e8aa6bb5f9e74af7ece2946b9f01ec22d3
BondarenkoOlexii <bondarenkoalosg@gmail.com>
2025-04-19 22:30:14 +0300

    Add RadioButtons to sort them by novelty, popularity, and number of votes

--- Interface.py
+++ Interface.py
@@ -15,7 +15,7 @@ def submit_button():
         if list_of_var[i].get() != 0:
             list_of_getting.append(labels[i])
 
-    js_file = main.api_request(list_of_getting, 1)
+    js_file = main.api_request(list_of_getting, 1, filter_buttons_check())
 
     title_list = main.films_titles(js_file)
 
@@ -26,7 +26,8 @@ def submit_button():
 
 def clear_checkbutton():
     check_buttons = [checkbutton_1, checkbutton_2, checkbutton_3, checkbutton_4,
-                     checkbutton_5, checkbutton_6, checkbutton_7, checkbutton_8]
+                     checkbutton_5, checkbutton_6, checkbutton_7, checkbutton_8,
+                    ]
 
     for btn in check_buttons:
         if btn.winfo_exists():
@@ -38,7 +39,20 @@ def list_box(list_of_names_films):
     for i in list_of_names_films:
         list_1.insert(END, i)
 
+def filter_buttons_check():
+    dict_of_filters_var = {
+        1: "popularity.desc",
+        2: "release_date.desc",
+        3: "revenue.desc",
+        4: "vote_count.desc"
+    }
+    filters_to_send = 0
+    filter = Var_Filter.get()
+    for i in dict_of_filters_var:
+        if i == filter:
+            filters_to_send = dict_of_filters_var[i]
 
+    return filters_to_send
 
 
 
@@ -46,7 +60,7 @@ def list_box(list_of_names_films):
 root = Tk()
 root.title("Window")
 root.geometry("1280x720")
-root.resizable(width=False, height=False)
+root.resizable(width=True, height=True)
 
 
 #  NOTEBOOK AND 3 FRAMES
@@ -67,7 +81,7 @@ list_1 = Listbox(frame_1, selectmode=EXTENDED, width=100, height=60, font=7)
 list_1.place(x=550, y=0)
 
 
-
+# Genres IntVar Checkbuttons
 var1 = IntVar()
 var2 = IntVar()
 var3 = IntVar()
@@ -77,8 +91,11 @@ var6 = IntVar()
 var7 = IntVar()
 var8 = IntVar()
 
+# Filter IntVar Radiobuttons
+Var_Filter = IntVar()
 
 
+# Genres Checkbutton
 checkbutton_1 = Checkbutton(frame_1, text="Action", variable=var1, onvalue=1, offvalue=0)
 checkbutton_2 = Checkbutton(frame_1, text="Drama", variable=var2, onvalue=1, offvalue=0)
 checkbutton_3 = Checkbutton(frame_1, text="Comedy", variable=var3, onvalue=1, offvalue=0)
@@ -88,14 +105,25 @@ checkbutton_6 = Checkbutton(frame_1, text="Thriller", variable=var6, onvalue=1, 
 checkbutton_7 = Checkbutton(frame_1, text="Sci-Fi", variable=var7, onvalue=1, offvalue=0)
 checkbutton_8 = Checkbutton(frame_1, text="Family", variable=var8, onvalue=1, offvalue=0)
 
-checkbutton_1.place(x=50, y=50)
-checkbutton_2.place(x=50, y=100)
-checkbutton_3.place(x=50, y=150)
-checkbutton_4.place(x=50, y=200)
-checkbutton_5.place(x=50, y=250)
-checkbutton_6.place(x=50, y=300)
-checkbutton_7.place(x=50, y=350)
-checkbutton_8.place(x=50, y=400)
+checkbutton_1.place(x=40, y=50)
+checkbutton_2.place(x=40, y=100)
+checkbutton_3.place(x=40, y=150)
+checkbutton_4.place(x=40, y=200)
+checkbutton_5.place(x=40, y=250)
+checkbutton_6.place(x=40, y=300)
+checkbutton_7.place(x=40, y=350)
+checkbutton_8.place(x=40, y=400)
+
+
+# Sorter Checkbutton
+radio_popular = Radiobutton(frame_1, text='Popular', variable=Var_Filter, value=1, command=filter_buttons_check)
+radio_recent = Radiobutton(frame_1, text='Recent', variable=Var_Filter, value=2, command=filter_buttons_check)
+radio_votes = Radiobutton(frame_1, text='Most Voted', variable=Var_Filter, value=4, command=filter_buttons_check)
+
+
+radio_popular.place(x=280, y=200)
+radio_recent.place(x=280, y=250)
+radio_votes.place(x=280, y=300)
 
 
 Submit_button = Button(frame_1, text="Submit", command=lambda: submit_button(), width=25, height=10)
--- main.py
+++ main.py
@@ -6,18 +6,24 @@ Key_API = "0ed0bb382f81d8c9f11cad42bf2d282b"
 link = "https://api.themoviedb.org/3/discover/movie?"
 genres_link = f"https://api.themoviedb.org/3/genre/movie/list?api_key={Key_API}&language=uk-UA"
 
+
+
 headeres = {
     "accept" : "application/json",
     "Authorization" : "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZWQwYmIzODJmODFkOGM5ZjExY2FkNDJiZjJkMjgyYiIsIm5iZiI6MTc0MzQxNDE1OC43ODEsInN1YiI6IjY3ZWE2MzhlNjFjYWMyYjNhM2Y2ZjdlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-1w7K5my6GNYPMLObQsHNbIOxdZ32wG2gwyLNCJcqso"
 }
 
-def api_request(list_of_genres, number_of_page):
+responce = requests.get(genres_link, headers=headeres)
+data = responce.json()
+print(data)
+def api_request(list_of_genres, number_of_page, filter_name):
     params = {
         "apikey": Key_API,
         "include_adult": "false",
         "language": "en-US",
         "page": number_of_page,
-        "with_genres": list_of_genres
+        "with_genres": list_of_genres,
+        "sort_by" : filter_name
     }
 
     main_responce = requests.get(link, headers=headeres, params=params)
